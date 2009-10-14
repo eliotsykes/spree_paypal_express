83ce65ec531d773caa823fd7893f42449ec9dd5e
Bryan Mahoney <bryan@goDynamo.com>
2009-10-14 08:31:27 -0400

    removed non-working files

--- app/models/app_configuration.rb
+++
@@ -1,9 +0,0 @@
-class AppConfiguration < Configuration
-
-  # signup for a developer account at https://developer.paypal.com or checkout Ryan Bate's screencast about getting an account
- 
-  preference :paypal_express_login, :string, :default => 'your_login'
-  preference :paypal_express_password, :string, :default => 'your_password'
-  preference :paypal_express_signature, :string, :default => 'your_signature'
-
-end
--- config/routes.rb
+++
@@ -1,2 +0,0 @@
-map.resources :orders, :member => { :shipping_method => :any, :payment => :any, :confirmation => :any,
-                                    :express_checkout => :any, :confirm => :any, :complete => :any }
